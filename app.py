66591d891e040142cf84873aa68bd345cf05ae41
Harsh Verma <Harsh>
2021-04-02 17:14:17 +0530

    added feature for v0.0.1

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pickle as pickle
 import math
 from scipy.sparse import hstack
 import json
+from nltk.sentiment import SentimentIntensityAnalyzer
 app = Flask(__name__)
 
 
@@ -12,9 +13,13 @@ def index():
 
 
 
-@app.route('/categoryId/<categoryId>/view_count/<view_count>/video_count/<video_count>/subscriber_count/<subscriber_count>')
-def profile(categoryId, view_count, video_count, subscriber_count):
-    likes = predict_likes(categoryId, view_count, video_count, subscriber_count)
+@app.route('/categoryId/<categoryId>/view_count/<view_count>/video_count/<video_count>/subscriber_count/<subscriber_count>/video_titile/<video_titile>/description/<description>')
+def profile(categoryId, view_count, video_count, subscriber_count, video_titile, description):
+    subscriber_count = int(subscriber_count)
+    categoryId = int(categoryId)
+    view_count = int(view_count)
+    
+    likes = predict_likes(categoryId, view_count, video_count, subscriber_count, video_titile, description)
     x = {
         "likes": likes
     }
@@ -27,9 +32,28 @@ def profile(categoryId, view_count, video_count, subscriber_count):
     # print(likes)
     return jsonify(y)
 
-def predict_likes (categoryId, view_count, video_count, subscriber_count):
 
+def find_sentiment(title, description, sid):
+    try:
+        ss = sid.polarity_scores(title)
+        ss2 = sid.polarity_scores(description)
 
+        title_sentiment = ss['compound']
+        description_sentiment = ss2['compound']
+        
+        # print(title_sentiment)
+        # print(description_sentiment)
+        
+        return [title_sentiment, description_sentiment]
+    except Exception as e:
+        print (e)
+        pass
+
+def predict_likes (categoryId, view_count, video_count, subscriber_count,video_titile, description):
+    sid = SentimentIntensityAnalyzer()
+    list_of_sentiment = find_sentiment(video_titile, description,sid)
+    title_sentiment = list_of_sentiment[0]
+    description_sentiment = list_of_sentiment[1]
 
     numerical_features = []
     categorical_features = []
@@ -49,14 +73,24 @@ def predict_likes (categoryId, view_count, video_count, subscriber_count):
         view_count,
         video_count,
         subscriber_count,
+        title_sentiment,
+        description_sentiment,
     ]) 
 
     categorical_features = []
     categorical_features.append(["None"])
+    bias = ""
+    if (subscriber_count < 5*10**5):
+        bias = "0"
+    elif (subscriber_count >= 5*10**5 and subscriber_count < 5*10**6):
+        bias = "1"
+    else:
+        bias = "2"
+    
 
-    with open('Models/my_numerical_encoder.pkl', 'rb') as fid:
+    with open("Models/Train" + bias + "/my_numerical_encoder.pkl", 'rb') as fid:
         numencoder = pickle.load(fid)
-    with open('Models/my_categorical_encoder.pkl', 'rb') as fid:
+    with open("Models/Train" + bias + "/my_categorical_encoder.pkl", 'rb') as fid:
         catencoder = pickle.load(fid)
 
 
@@ -67,7 +101,7 @@ def predict_likes (categoryId, view_count, video_count, subscriber_count):
     X = hstack([numerical_features2, categorical_features2])
 
 
-    with open('Models/my_dumped_classifier.pkl', 'rb') as fid:
+    with open("Models/Train" + bias + "/my_dumped_classifier.pkl", 'rb') as fid:
         model = pickle.load(fid)
         
     y_pred = [] 
